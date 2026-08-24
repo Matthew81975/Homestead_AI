4757909f89da8a978835187f57ab5a50ac288301
Matthew81975 <123055165+Matthew81975@users.noreply.github.com>
2026-08-24 10:23:44 -0500

    feat: expose multiphysics simulation API

--- hcs_ai/design_platform/__init__.py
+++ hcs_ai/design_platform/__init__.py
@@ -5,10 +5,12 @@ from .geometry import RectOpening, StraightWallGeometry
 from .loads import WallLoads
 from .mass_walls import EngineeringValue, EarthbagAssembly, RubbleAssembly
 from .results import CheckResult, PierResult, WallAnalysisResult
+from .simulation import PhysicsDomain, SimulationCase, SimulationLoad
 from .units import Quantity, from_si, to_si
 
 __all__ = [
     'Quantity', 'to_si', 'from_si', 'RectOpening', 'StraightWallGeometry',
     'EngineeringValue', 'EarthbagAssembly', 'RubbleAssembly', 'WallLoads',
     'CheckResult', 'PierResult', 'WallAnalysisResult', 'analyze_straight_mass_wall',
+    'PhysicsDomain', 'SimulationCase', 'SimulationLoad',
 ]
