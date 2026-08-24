56b238b736f214316d000555ce113916a462df41
Matthew81975 <123055165+Matthew81975@users.noreply.github.com>
2026-08-24 10:23:38 -0500

    feat: expose visualization layer and view APIs

--- hcs_ai/visualization/__init__.py
+++ hcs_ai/visualization/__init__.py
@@ -1,3 +1,10 @@
 from .scene import Scene, SceneEdge, SceneNode, Transform
+from .layers import DEFAULT_LAYERS, LayerRegistry, LayerState, filter_scene
+from .presets import build_preset
+from .views import OrthographicView, ViewPlane
 
-__all__ = ['Scene', 'SceneEdge', 'SceneNode', 'Transform']
+__all__ = [
+    'Scene', 'SceneEdge', 'SceneNode', 'Transform',
+    'DEFAULT_LAYERS', 'LayerRegistry', 'LayerState', 'filter_scene',
+    'build_preset', 'OrthographicView', 'ViewPlane',
+]
