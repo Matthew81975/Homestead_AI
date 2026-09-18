5328cba7d2093a71e18a3ec1c5799697424b3154
Matthew81975 <123055165+Matthew81975@users.noreply.github.com>
2026-09-18 14:57:35 -0500

    refactor: export Local Codex feature API

--- hcs_ai/tabs/local_codex/__init__.py
+++ hcs_ai/tabs/local_codex/__init__.py
@@ -1,5 +1,29 @@
-"""Local Codex HCS tab package."""
+"""Local Codex HCS feature package."""
 
+from .controller import LocalCodexControllerMixin
+from .presentation import (
+    LOCAL_CODEX_TAB_TITLE,
+    REQUIRED_CONTROL_NAMES,
+    format_local_codex_event,
+    local_codex_control_states,
+    local_codex_search_ranges,
+    ordered_local_codex_events,
+    should_follow_local_codex_log,
+)
 from .tab import LOCAL_CODEX_TAB, build_local_codex_tab
+from .view import LocalCodexGuiMixin, LocalCodexViewMixin
 
-__all__ = ["LOCAL_CODEX_TAB", "build_local_codex_tab"]
+__all__ = [
+    "LOCAL_CODEX_TAB",
+    "LOCAL_CODEX_TAB_TITLE",
+    "REQUIRED_CONTROL_NAMES",
+    "LocalCodexControllerMixin",
+    "LocalCodexGuiMixin",
+    "LocalCodexViewMixin",
+    "build_local_codex_tab",
+    "format_local_codex_event",
+    "local_codex_control_states",
+    "local_codex_search_ranges",
+    "ordered_local_codex_events",
+    "should_follow_local_codex_log",
+]
